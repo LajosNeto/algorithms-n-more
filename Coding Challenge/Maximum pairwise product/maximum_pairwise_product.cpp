6f4e45b4250dc0c8399798bec45fbaf03ee5a7ad
Lajos Neto <lajosnetogit@gmail.com>
2018-02-21 15:11:51 -0400

    Maximum Pairwise - final adjustments for corner cases

--- Coding Challenge/Maximum pairwise product/maximum_pairwise_product.cpp
+++ Coding Challenge/Maximum pairwise product/maximum_pairwise_product.cpp
@@ -14,27 +14,26 @@ using std::cin;
 using std::cout;
 using std::max;
 
-int MaxPairwiseProduct(const vector<int>& numbers){
-    int product = 0;
+int64_t MaxPairwiseProduct(const vector<int64_t>& numbers){
     int vec_size = numbers.size();
-    for(int i=0; i<vec_size; ++i){
-        for (int j=i+1; j<vec_size; ++j){
-            if(numbers[i] != numbers[j]){
-                product = max(product, numbers[i]*numbers[j]);
-            }
-        }
-    }
-    return product;
+    int64_t biggerA = 0, biggerB = 0;
+    int biggerA_position;
+    for(int i=0; i<vec_size; ++i) if(numbers[i] > biggerA){
+        biggerA = numbers[i];
+        biggerA_position = i;
+    } 
+    for(int j=0; j<vec_size; ++j) if(numbers[j] > biggerB and j != biggerA_position) biggerB = numbers[j];
+    return biggerA*biggerB;
 }
 
 int main(){
-    int n = 0;
+    int n;
     cin >> n;
-    vector<int> numbers(10);
-    for(int i=0; i<n; i++){
+    vector<int64_t> numbers(n);
+    for(int i=0; i<n; ++i){
         cin >> numbers[i];
     }
-    int max_product = MaxPairwiseProduct(numbers);
+    int64_t max_product = MaxPairwiseProduct(numbers);
     cout << max_product << "\n";
     return 0;
 }
