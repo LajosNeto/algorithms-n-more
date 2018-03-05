395b4410faa8d884c8d717be8a2c101405dadde6
Lajos Neto <lajosnetogit@gmail.com>
2018-03-04 23:56:21 -0400

    Array C - adding array C code

---
+++ Data Structures/Array/C/array.h
@@ -0,0 +1,18 @@
+/** @file array.h
+* 
+* @brief Dynamic array implementation
+*
+*@author Lajos Onodi Neto
+*/
+
+#ifndef PROJECT_ARRAY_H
+#define PROJECT_ARRAY_H
+
+#include <assert.h>
+#include <stdbool.h>
+
+
+
+
+
+#endif  // PROJECT_ARRAY_H
