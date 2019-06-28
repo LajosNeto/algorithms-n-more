602b7c4aeae4dca1dfba9f10ab77903a5a584634
Lajos Neto <lajosnetogit@gmail.com>
2019-06-27 22:12:28 -0400

    Add Queue array based implementation and dequeue method
    Add Queue array based implementation.
    Add dequeue method for queue based on linked list implementation.
    Add dequeue method for queue based on array implementation.
    Add tests for dequeue method and enqueue method from queue based on array implementation.
    Add tests for dequeue method from queue based on linked list implementation.

--- Data Structures/Queue/python/queue.py
+++ Data Structures/Queue/python/queue.py
@@ -8,10 +8,11 @@ Queue data structure implementation
 
 import sys
 
+# TODO : Add AbstractQueue implementation
 
 class _Node:
     """
-    Simple node used for representing values inside the queue based on linked list
+    Simple node used for representing values inside the queue based on linked list.
     """
     def __init__(self, value):
         self.value = value
@@ -19,15 +20,15 @@ class _Node:
 
 class LinkedListQueue:
     """
-    Queue implementation using linked list
+    Queue implementation using linked list.
     """
     def __init__(self):
-        self.head = None
-        self.tail = None
-        self.size = 0
+        self._head = None
+        self._tail = None
+        self._size = 0
     
     def __iter__(self):
-        iterator = self.head
+        iterator = self._head
         while True:
             if iterator is None:
                 return
@@ -35,14 +36,76 @@ class LinkedListQueue:
             iterator = iterator.next
             
     def is_empty(self):
-        return self.size == 0
+        return self._size == 0
     
     def enqueue(self, value):
         new_node = _Node(value)
         if self.is_empty():
-            self.head = new_node
-            self.tail = new_node
-            self.size += 1
+            self._head = new_node
+            self._tail = new_node
         else:
-            self.tail.next = new_node
-            self.tail = new_node
+            self._tail.next = new_node
+            self._tail = new_node
+        self._size += 1
+    
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("Empty queue")
+        dequeued_value = self._head.value
+        if self._tail is self._head:
+            self._tail = None
+            self._head = None
+        else:
+            self._head = self._head.next
+        self._size -= 1
+        return dequeued_value
+    
+class ArrayQueue:
+    """
+    Queue implementation using arrays.
+    Even though arrays in Python being dynamic, here we 
+    simulate as if they were static.
+    """
+
+    def __init__(self):
+        self._array = [None] * 5
+        self._head = 0
+        self._tail = 0
+        self._size = 0
+    
+    def __iter__(self):
+        iterator = self._head
+        while True:
+            if iterator == self._tail:
+                return
+            yield self._array[iterator]
+            iterator += 1
+    
+    def is_empty(self):
+        return self._size == 0
+    
+    def enqueue(self, value):
+        if self._tail == len(self._array):
+            self._expand()
+        self._array[self._tail] = value
+        self._tail += 1
+        self._size += 1
+    
+    def dequeue(self):
+        if(self.is_empty()):
+            raise IndexError("Empty queue")
+        dequeued_value = self._array[self._head]
+        self._array[self._head] = None
+        self._head += 1
+        self._size -= 1
+        self._shrink()
+        return dequeued_value
+    
+    def _expand(self):
+        self._array += [None] * 5
+    
+    def _shrink(self):
+        if(self._size/len(self._array) >= 2):
+            aux_array = [i for i in a if i != None]
+            self._array = aux_array
+            self._size = len(self._array)
--- Data Structures/Queue/python/test_queue.py
+++ Data Structures/Queue/python/test_queue.py
@@ -9,12 +9,14 @@ Queue data structure tests
 import sys
 import unittest
 from queue import LinkedListQueue
+from queue import ArrayQueue
 
 
 class QueueTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(QueueTest, self).__init__(*args, **kwargs)
         self.ll_queue = LinkedListQueue()
+        self.arr_queue = ArrayQueue()
     
     def test_ll_queue_enqueue(self):
         self.ll_queue.enqueue(10)
@@ -24,3 +26,52 @@ class QueueTest(unittest.TestCase):
         self.assertEqual([i for i in self.ll_queue], [10,20,30,40])
         self.ll_queue.enqueue(50)
         self.assertEqual([i for i in self.ll_queue], [10,20,30,40,50])
+    
+    def test_ll_queue_dequeue(self):
+        self.ll_queue.enqueue(10)
+        self.ll_queue.enqueue(20)
+        self.ll_queue.enqueue(30)
+        self.ll_queue.enqueue(40)
+        self.assertEqual(self.ll_queue.dequeue(), 10)
+        self.assertEqual([i for i in self.ll_queue], [20,30,40])
+        self.assertEqual(self.ll_queue.dequeue(), 20)
+        self.assertEqual(self.ll_queue.dequeue(), 30)
+        self.assertEqual(self.ll_queue.dequeue(), 40)
+        self.assertRaises(IndexError, self.ll_queue.dequeue)
+        self.ll_queue.enqueue(10)
+        self.ll_queue.enqueue(20)
+        self.assertEqual(self.ll_queue.dequeue(), 10)
+    
+    def test_arr_queue_enqueue(self):
+        self.arr_queue.enqueue(10)
+        self.arr_queue.enqueue(20)
+        self.arr_queue.enqueue(30)
+        self.arr_queue.enqueue(40)
+        self.assertEqual([i for i in self.arr_queue], [10,20,30,40])
+        self.arr_queue.enqueue(50)
+        self.assertEqual([i for i in self.arr_queue], [10,20,30,40,50])
+        self.arr_queue.enqueue(60)
+        self.arr_queue.enqueue(70)
+        self.assertEqual([i for i in self.arr_queue], [10,20,30,40,50,60,70])
+        self.arr_queue.enqueue(80)
+        self.arr_queue.enqueue(90)
+        self.arr_queue.enqueue(100)
+        self.arr_queue.enqueue(110)
+        self.arr_queue.enqueue(120)
+        self.arr_queue.enqueue(130)
+        self.assertEqual([i for i in self.arr_queue], [10,20,30,40,50,60,70,80,90,100,110,120,130])
+    
+    def test_arr_queue_dequeue(self):
+        self.arr_queue.enqueue(10)
+        self.arr_queue.enqueue(20)
+        self.arr_queue.enqueue(30)
+        self.arr_queue.enqueue(40)
+        self.assertEqual(self.arr_queue.dequeue(), 10)
+        self.assertEqual([i for i in self.arr_queue], [20,30,40])
+        self.assertEqual(self.arr_queue.dequeue(), 20)
+        self.assertEqual(self.arr_queue.dequeue(), 30)
+        self.assertEqual(self.arr_queue.dequeue(), 40)
+        self.assertRaises(IndexError, self.arr_queue.dequeue)
+        self.arr_queue.enqueue(10)
+        self.arr_queue.enqueue(20)
+        self.assertEqual(self.arr_queue.dequeue(), 10)
