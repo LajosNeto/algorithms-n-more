a50c764917f75cdb069cf092a42bc3a7f58b3127
Lajos Neto <lajosnetogit@gmail.com>
2019-07-13 18:14:07 -0400

    Add insert method
    Add method for inserting a value at a given index position.
    Add tests for new insert method.
    Add __iter__ method implementation.

--- data-structures/linked-list/python/linked_list.py
+++ data-structures/linked-list/python/linked_list.py
@@ -4,3 +4,50 @@ Linked List data structure implementation
 
 # Author: 
 # Lajos Neto <lajosnetogit@gmail.com>
+
+
+class _Node:
+    """
+    Simple node used for representing values 
+    inside the linked list.
+    """
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    """
+    Linked List main class where all value nodes
+    are stored within.
+    """
+    def __init__(self):
+        self._head = None
+        self._tail = None
+        self._size = 0
+    
+    def __iter__(self):
+        iterator = self._head
+        while True:
+            if iterator is None:
+                return
+            yield iterator.value
+            iterator = iterator.next
+    
+    def insert(self, index, value):
+        """Insert a vule at a given index position"""
+        new_node = _Node(value)
+        if None in (self._head, self._tail):
+            self._head = new_node
+            self._tail = new_node
+        elif index >= self._size or index < 0:
+            raise IndexError
+        elif index == 0:
+            new_node.next = self._head
+            self._head = new_node
+        else:
+            aux_pointer = self._head
+            for i in range(index-1):
+                aux_pointer = aux_pointer.next
+            new_node.next = aux_pointer.next
+            aux_pointer.next = new_node
+        self._size += 1
--- data-structures/linked-list/python/test_linked_list.py
+++ data-structures/linked-list/python/test_linked_list.py
@@ -4,3 +4,25 @@ Linked List data structure tests
 
 # Author: 
 # Lajos Neto <lajosnetogit@gmail.com>
+
+
+import pytest
+from linked_list import LinkedList
+
+
+def test_insert():
+    ll = LinkedList()
+    ll.insert(0, 30)
+    assert [i for i in ll] == [30]
+    ll.insert(0, 20)
+    assert [i for i in ll] == [20, 30]
+    ll.insert(0, 10)
+    assert [i for i in ll] == [10, 20, 30]
+    with pytest.raises(IndexError):
+        ll.insert(3, 40)
+    ll.insert(2, 40)
+    assert [i for i in ll] == [10, 20, 40, 30]
+    ll.insert(1, 10)
+    assert [i for i in ll] == [10, 10, 20, 40, 30]
+    ll.insert(3, 90)
+    assert [i for i in ll] == [10, 10, 20, 90, 40, 30]
