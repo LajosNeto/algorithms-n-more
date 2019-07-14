92f59ca0d7659fe6bae6db672201c460f2ecdc38
Lajos Neto <lajosnetogit@gmail.com>
2019-07-14 01:04:34 -0400

    Add new empty() method for checking if stack is empty

--- data-structures/stack/python/stack.py
+++ data-structures/stack/python/stack.py
@@ -1,6 +1,29 @@
 """
-Stack data structure implementation
+Stack data structure implementation based on linked lists
 """
 
 # Author: 
 # Lajos Neto <lajosnetogit@gmail.com>
+
+
+class _Node:
+    """
+    Simple node used for representing values 
+    inside the stack.
+    """
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+class Stack:
+    """
+    Stack data structure.
+    """
+    def __init__(self):
+        self._top = None
+        self._bottom = None
+        self._size = 0
+    
+    def empty(self):
+        """Check if the list is empty"""
+        return self._size == 0
--- data-structures/stack/python/test_stack.py
+++ data-structures/stack/python/test_stack.py
@@ -4,3 +4,11 @@ Stack data structure tests
 
 # Author: 
 # Lajos Neto <lajosnetogit@gmail.com>
+
+
+import pytest
+from stack import Stack
+
+def test_empty():
+    st = Stack()
+    assert st.empty() == True
