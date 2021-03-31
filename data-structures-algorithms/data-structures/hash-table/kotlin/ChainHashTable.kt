204ffb2889cd64774f3291c923dc4cf75d768a82
Lajos Onodi Neto <lajosnetogit@gmail.com>
2021-03-30 23:02:55 -0300

    Add new Chain Hash Table(kotlin) functions
    Add new hash function.
    Add put function.
    Add base prime numbers on comapanion object.
    Add test print function for collisions amound debug.

--- data-structures-algorithms/data-structures/hash-table/kotlin/ChainHashTable.kt
+++ data-structures-algorithms/data-structures/hash-table/kotlin/ChainHashTable.kt
@@ -1,30 +1,66 @@
 /**
  * Hash Table implementation based on chaining approach for collision resolution.
+ *
+ * Resources for implementation and values choices :
  * Prehash approach : https://web.stanford.edu/class/archive/cs/cs106b/cs106b.1164/handouts/8-Hashing.pdf
  * Prehash approach : https://cp-algorithms.com/string/string-hashing.html
  * Prehash approach : http://www.cse.yorku.ca/~oz/hash.html
+ * Universal hashing : https://www.cs.cmu.edu/~avrim/451f11/lectures/lect1004.pdf
+ * Universal hashing primes table : https://planetmath.org/goodhashtableprimes
  *
  * Author : Lajos Neto.
  * Email : lajosnetogit@gmail.com
  */
 
 import kotlin.math.pow
+import kotlin.random.Random
 
-class HashTable<K, V>(val hashFunction: String = "universal") {
-    var tableSize = 8
-    var table = IntArray(tableSize)
+class ChainHashTable<K, V>(val hashFunction: String = "universal") {
+    private var tableSize = INITIAL_TABLE_PRIME_SIZE
+    private var valueCount = 0
+    private var table = Array(tableSize) { mutableListOf<Pair<K, V>>() }
 
-    fun prehash(key: K) =
-        key.toString().mapIndexed { index, char -> char.toInt() * 31.0.pow(index + 1) }.sum().toInt()
+    fun preHash(key: K) =
+        key.toString().mapIndexed { index, char -> char.toInt() * PRE_HASH_PRIME.pow(index + 1) }.sum()
 
     fun hash(key: K): Int {
-        return 0
+        val preHashKey = preHash(key)
+        val a = Random(RANDOM_SEED).nextInt(0, UNIVERSAL_HASH_PRIME - 1)
+        val b = Random(RANDOM_SEED).nextInt(0, UNIVERSAL_HASH_PRIME - 1)
+        return ((((a*preHashKey) + b) % UNIVERSAL_HASH_PRIME) % tableSize).toInt()
     }
 
-//    private fun expand() {}
+    fun put(key: K, value: V) {
+        val keyHash = hash(key)
+        table[keyHash].add(Pair(key, value))
+        valueCount += 1
+        if (valueCount > tableSize) expand()
+    }
+
+    private fun expand() {}
 //    private fun shrink() {}
-//    fun get(key: T): V { return V }
-//    fun has(key: T): Boolean { return true }
-//    fun put(key: K, value: V) {}
+//    private fun closestPrime(after: Int): Int {}
+//    fun get(key: K): V { return }
+//    fun has(key: K): Boolean { return true }
 //    fun remove(key: K) {}
+
+    fun testPrint() {
+        var collidedIndexes = 0
+        var collisionCount = 0
+        table.forEachIndexed { index, bucket ->
+            println("Bucket $index : $bucket")
+            if (bucket.size > 1) {
+                collidedIndexes +=1
+                collisionCount += bucket.size
+            }
+        }
+        println("==== COLLIDED INDEXES : $collidedIndexes with TOTAL COLLISIONS : ${collisionCount - collidedIndexes} ====")
+    }
+
+    companion object {
+        const val INITIAL_TABLE_PRIME_SIZE = 11
+        const val RANDOM_SEED = 42
+        const val PRE_HASH_PRIME = 31.0
+        const val UNIVERSAL_HASH_PRIME = 3079
+    }
 }
