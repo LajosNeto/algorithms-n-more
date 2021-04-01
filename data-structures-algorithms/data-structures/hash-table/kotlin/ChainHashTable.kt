c63c6d484c2be446a927c53cee6ce95cee38e4b5
Lajos Onodi Neto <lajosnetogit@gmail.com>
2021-04-01 16:39:42 -0300

    Add new Chain Hash Table(kotlin) functions
    Add expand feature.
    Add shrink feature.
    Add remove function.
    Add keyCount function.
    Add tableSize function.
    Add tests for new functions/features.

--- data-structures-algorithms/data-structures/hash-table/kotlin/ChainHashTable.kt
+++ data-structures-algorithms/data-structures/hash-table/kotlin/ChainHashTable.kt
@@ -16,33 +16,67 @@ import kotlin.math.pow
 import kotlin.random.Random
 
 class ChainHashTable<K, V>(val hashFunction: String = "universal") {
-    private var tableSize = INITIAL_TABLE_PRIME_SIZE
-    private var valueCount = 0
+    private var tableSize = MIN_TABLE_SIZE
+    private var keyCount = 0
     private var table = Array(tableSize) { mutableListOf<Pair<K, V>>() }
 
     fun preHash(key: K) =
         key.toString().mapIndexed { index, char -> char.toInt() * PRE_HASH_PRIME.pow(index + 1) }.sum()
 
     fun hash(key: K): Int {
-        val preHashKey = preHash(key)
         val a = Random(RANDOM_SEED).nextInt(0, UNIVERSAL_HASH_PRIME - 1)
         val b = Random(RANDOM_SEED).nextInt(0, UNIVERSAL_HASH_PRIME - 1)
-        return ((((a*preHashKey) + b) % UNIVERSAL_HASH_PRIME) % tableSize).toInt()
+        return ((((a*preHash(key)) + b) % UNIVERSAL_HASH_PRIME) % tableSize).toInt()
     }
 
     fun put(key: K, value: V) {
-        val keyHash = hash(key)
-        table[keyHash].add(Pair(key, value))
-        valueCount += 1
-        if (valueCount > tableSize) expand()
+        if (!has(key)) {
+            table[hash(key)].add(Pair(key, value))
+            keyCount += 1
+            if (keyCount >= tableSize) expand()
+        }
     }
 
-    private fun expand() {}
-//    private fun shrink() {}
-//    private fun closestPrime(after: Int): Int {}
+    fun remove(key: K) {
+        val keyHash = hash(key)
+        table[keyHash].forEachIndexed { index, node ->
+            if (node.first == key) {
+                table[keyHash].removeAt(index)
+                keyCount -= 1
+                if (keyCount <= tableSize / 4) shrink()
+                return
+            }
+        }
+    }
+
+    private fun expand() { reshape(tableSize*2) }
+
+    private fun shrink() { if (tableSize > MIN_TABLE_SIZE) reshape(tableSize/2) }
+
+    private fun reshape(newSize: Int) {
+        tableSize = newSize
+        val resizedTable = Array(tableSize) { mutableListOf<Pair<K, V>>() }
+        table.forEach { slot ->
+            slot.forEach { node ->
+                val keyHash = hash(node.first)
+                resizedTable[keyHash].add(node)
+            }
+        }
+        table = resizedTable
+    }
+
+    fun has(key: K): Boolean {
+        table[hash(key)].forEach { node ->
+            if (node.first == key) return true
+        }
+        return false
+    }
+
+    fun keyCount() = keyCount
+
+    fun tableSize() = tableSize
+
 //    fun get(key: K): V { return }
-//    fun has(key: K): Boolean { return true }
-//    fun remove(key: K) {}
 
     fun testPrint() {
         var collidedIndexes = 0
@@ -58,7 +92,7 @@ class ChainHashTable<K, V>(val hashFunction: String = "universal") {
     }
 
     companion object {
-        const val INITIAL_TABLE_PRIME_SIZE = 11
+        const val MIN_TABLE_SIZE = 8
         const val RANDOM_SEED = 42
         const val PRE_HASH_PRIME = 31.0
         const val UNIVERSAL_HASH_PRIME = 3079
--- data-structures-algorithms/data-structures/hash-table/kotlin/TestChainHashTable.kt
+++ data-structures-algorithms/data-structures/hash-table/kotlin/TestChainHashTable.kt
@@ -6,18 +6,97 @@
  */
 
 import org.junit.Test
+import kotlin.random.Random
 
 class HashTableTest {
 
     @Test
     fun preHashTest() {
-        val hashTable = HashTable<String, Int>()
-        assert(hashTable.prehash("a") == 3007)
-        assert(hashTable.prehash("b") == 3038)
-        assert(hashTable.prehash("c") == 3069)
-        assert(hashTable.prehash("d") == 3100)
-        assert(hashTable.prehash("abcd") == 95398594)
-        assert(hashTable.prehash("abdc") != 95398594)
+        val hashTable = ChainHashTable<String, Int>()
+        assert(hashTable.preHash("a").toInt() == 3007)
+        assert(hashTable.preHash("b").toInt() == 3038)
+        assert(hashTable.preHash("c").toInt() == 3069)
+        assert(hashTable.preHash("d").toInt() == 3100)
+        assert(hashTable.preHash("abcd").toInt() == 95398594)
+        assert(hashTable.preHash("abdc").toInt() != 95398594)
+    }
+
+    @Test
+    fun hasKeyTest() {
+        val hashTable = ChainHashTable<String, Int>()
+        hashTable.put("abcd", 1)
+        assert(hashTable.has("abcd"))
+        assert(!hashTable.has("abcde"))
+        hashTable.put("abcde", 1)
+        assert(hashTable.has("abcde"))
+        hashTable.put("dcba", 1)
+        assert(hashTable.has("dcba"))
+    }
+
+    @Test
+    fun removeTest() {
+        val hashTable = ChainHashTable<Int, String>()
+        (1..7).forEach { i -> hashTable.put(i, Random.nextInt().toString()) }
+        assert(hashTable.keyCount() == 7)
+        hashTable.remove(1)
+        assert(hashTable.keyCount() == 6)
+        hashTable.remove(1)
+        assert(hashTable.keyCount() == 6)
+        hashTable.remove(2)
+        assert(hashTable.keyCount() == 5)
+        hashTable.remove(3)
+        assert(hashTable.keyCount() == 4)
+    }
+
+    @Test
+    fun expandTest() {
+        val hashTable = ChainHashTable<Int, String>()
+        assert(hashTable.tableSize() == 8)
+        (1..7).forEach { i -> hashTable.put(i, Random.nextInt().toString()) }
+        assert(hashTable.tableSize() == 8)
+        hashTable.put(8, Random.nextInt().toString())
+        assert(hashTable.tableSize() == 16)
+        (9..15).forEach { i -> hashTable.put(i, Random.nextInt().toString()) }
+        assert(hashTable.tableSize() == 16)
+        hashTable.put(16, Random.nextInt().toString())
+        assert(hashTable.tableSize() == 32)
+    }
+
+    @Test
+    fun shrinkTest() {
+        val hashTable = ChainHashTable<Int, String>()
+        assert(hashTable.tableSize() == 8)
+        (1..128).forEach { i -> hashTable.put(i, Random.nextInt().toString()) }
+        assert(hashTable.tableSize() == 256)
+        (64..128).forEach { i -> hashTable.remove(i) }
+        assert(hashTable.tableSize() == 128)
+        (32..64).forEach { i -> hashTable.remove(i) }
+        assert(hashTable.tableSize() == 64)
+        (16..32).forEach { i -> hashTable.remove(i) }
+        assert(hashTable.tableSize() == 32)
+        (8..16).forEach { i -> hashTable.remove(i) }
+        assert(hashTable.tableSize() == 16)
+        (4..8).forEach { i -> hashTable.remove(i) }
+        assert(hashTable.tableSize() == 8)
+        (1..4).forEach { i -> hashTable.remove(i) }
+        assert(hashTable.tableSize() == 8)
+    }
+
+    @Test
+    fun expandAndShrinkTest() {
+        val hashTable = ChainHashTable<Int, String>()
+        (1..128).forEach { i -> hashTable.put(i, Random.nextInt().toString()) }
+        assert(hashTable.tableSize() == 256)
+        (64..128).forEach { i -> hashTable.remove(i) }
+        assert(hashTable.tableSize() == 128)
+        (64..128).forEach { i -> hashTable.put(i, Random.nextInt().toString()) }
+        assert(hashTable.tableSize() == 256)
+        (32..128).forEach { i -> hashTable.remove(i) }
+        assert(hashTable.tableSize() == 64)
+        (32..64).forEach { i -> hashTable.put(i, Random.nextInt().toString()) }
+        assert(hashTable.tableSize() == 128)
+        (16..64).forEach { i -> hashTable.remove(i) }
+        assert(hashTable.tableSize() == 32)
     }
 
 }
